9a7eca16ecf364adb4b5c92d5cb029f45c88c695
laiwei <laiwei.ustc@gmail.com>
2013-08-05 21:21:32 +0800

    update

--- install.sh
+++ install.sh
@@ -30,7 +30,12 @@ install_file .dircolors
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 
-
-
-
-
+if [ -d "$GOROOT" ]; then
+    mkdir -p $HOME/.vim/ftdetect
+    mkdir -p $HOME/.vim/syntax
+    mkdir -p $HOME/.vim/autoload/go
+    ln -s $GOROOT/misc/vim/ftdetect/gofiletype.vim $HOME/.vim/ftdetect/
+    ln -s $GOROOT/misc/vim/syntax/go.vim $HOME/.vim/syntax
+    ln -s $GOROOT/misc/vim/autoload/go/complete.vim $HOME/.vim/autoload/go
+    echo "syntax on" >> $HOME/.vimrc
+fi
